9a17fe335ac0022f3b343420ddc317fee665b367
AzimJaan Akhtamov <azimaxtamov2oo2@mail.ru>
2024-05-07 23:06:05 +0500

    fixed

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,4 @@
 import 'package:get_topik_korean_quiz/tools/file_importer.dart';
-
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -14,7 +12,9 @@ class HomeScreen extends StatelessWidget {
             width: MediaQuery.sizeOf(context).width,
             child: Column(
               children: [
+                
                 const TopBarGetTopik(),
+                /// Top ///
                 const SizedBox(height: 20),
                 NeumorphContainer(
                   nchild: const Text(
@@ -26,6 +26,16 @@ class HomeScreen extends StatelessWidget {
                         Navigator.of(context).pushNamed(RouteName.getTopik1),
                 ),
                 const SizedBox(height: 20),
+
+                NeumorphContainer(
+                  nchild: const Text(
+                    "초급 2",
+                    style: bannerText,
+                  ),
+                  countNumchild: const Text(" 300 ta so'z"),
+                  onTap: () =>
+                        Navigator.of(context).pushNamed(RouteName.getTopik1),
+                ),
               ],
             ),
           ),
